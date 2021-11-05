2e5b21c1b6add52a7b3f73f3ee66d0a8b34ef130
pmb <pascal.bayer1999@gmail.com>
2021-11-05 23:08:35 +0100

    enabled debug mode

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return 'Hello Reiner'
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
